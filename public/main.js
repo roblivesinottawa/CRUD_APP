d71d5e15b9c6385a96824845b55e95e59af577be
Robson Trajano <59901422+roblivesinottawa@users.noreply.github.com>
2021-05-18 14:05:41 -0400

    "added index.html, delete route"

--- public/main.js
+++ public/main.js
@@ -1,12 +1,41 @@
-const update = document.querySelector('#update-button')
+const update = document.querySelector("#update-button");
+const deleteButton = document.querySelector("#delete-button");
+const messageDiv = document.querySelector("#message");
 
-update.addEventListener('click', _ => {
-    fetch('/movies', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            name: 'Darth Vader',
-            movie: 'The Return Of The Jedi'
-        })
+update.addEventListener("click", (_) => {
+  fetch("/movies", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vader",
+      movie: "The Return Of The Jedi",
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
     })
-})
+    .then((response) => {
+      window.location.reload(true);
+    });
+});
+
+deleteButton.addEventListener("click", (_) => {
+  fetch("/movies", {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vader",
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+    })
+    .then((response) => {
+      if (response === "No movie to delete") {
+        messageDiv.textContent = "No Star Wars movie to delete";
+      } else {
+        window.location.reload(true);
+      }
+    })
+    .catch(console.error);
+});
--- server.js
+++ server.js
@@ -1,61 +1,81 @@
-const express = require('express')
-const PORT = process.env.PORT || 3000
-const app = express()
-const MongoClient = require('mongodb').MongoClient
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 // connect to mongodb
-MONGO_DB_URI = 'mongodb+srv://dbUser:1029384756@cluster1.gskfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// MongoClient.connect(MONGO_DB_URI, {useUnifiedTopology: true }, (err, client) => {
-//     err ? console.error(err) : console.log('CONNECTED TO THE DATABASE!')
-// })
+MONGO_DB_URI =
+  "mongodb+srv://dbUser:1029384756@cluster1.gskfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 // using promises instead of callbacks
-MongoClient.connect(MONGO_DB_URI, {useUnifiedTopology: true })
-.then(client => {
-    console.log('CONNECTED TO THE DATABASE!')
-    const db = client.db('star-wars-movies')
-    const moviesCollection = db.collection('movies')
-    
-    app.set('view engine', 'ejs')
+MongoClient.connect(MONGO_DB_URI, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("CONNECTED TO THE DATABASE!");
+    const db = client.db("star-wars-movies");
+    const moviesCollection = db.collection("movies");
     // middlewares
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(express.static('public'))
-    
+    app.set("view engine", "ejs");
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static("public"));
+
     // routes
-    app.get('/', (req, res) => {
-        db.collection('movies').find().toArray()
-        .then(movies => {
-            res.render('index.ejs', { movies: movies})
+
+    // GET
+    app.get("/", (req, res) => {
+      db.collection("movies")
+        .find()
+        .toArray()
+        .then((movies) => {
+          res.render("index.ejs", { movies: movies });
         })
-        .catch(error => console.error(error))
-    })
+        .catch(/* ... */);
+    });
 
-    app.post('/movies', (req, res) => {
-        moviesCollection.insertOne(req.body)
-        .then(result => {
-            res.redirect('/')
-        }).catch(error => console.error(error))
+    // POST
+    app.post("/movies", (req, res) => {
+      moviesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
 
-    app.put('/movies', (req, res) => {
-        moviesCollection.findOneAndUpdate(
-            {name: 'Return Of The Jedi'},
+      // PUT
+      app.put("/movies", (req, res) => {
+        moviesCollection
+          .findOneAndUpdate(
+            { name: "Return Of The Jedi" },
             {
-                $set: {
-                    name: req.body.name,
-                    movie: req.body.movie
-                }
+              $set: {
+                name: req.body.name,
+                movie: req.body.movie,
+              },
             },
-           { upsert: true }
-        ).then(result => {
-            res.json('Success!')
-        })
-        .catch(error => console.error(error))
-    })
-    
-    app.listen(PORT, () => console.log(`Server listening at localhost:${PORT}`))
+            { upsert: true },
+          )
+          .then((result) => {
+            res.json("Success!");
+          })
+          .catch((error) => console.error(error));
+      });
 
+      // DELETE
+      app.delete("/movies", (req, res) => {
+        moviesCollection
+          .deleteOne({ name: req.body.name })
+          .then((result) => {
+            if (result.deletedCount === 0) {
+              return res.json("No movie to delete");
+            }
+            res.json("Deleted movie");
+          })
+          .catch((error) => console.error(error));
+      });
 
-})
-.catch(console.error)
-})
+      app.listen(PORT, () =>
+        console.log(`Server listening at localhost:${PORT}`),
+      );
+    });
+  })
+  .catch(console.error);
